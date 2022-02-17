a7a18fdeb57f313a04484e5d326b364e87e81492
Gabriel-Leao <gabriellsilva1200@outlook.com.br>
2022-02-17 12:19:27 -0300

    While

---
+++ while.js
@@ -0,0 +1,14 @@
+let passo = 1
+
+while (passo <=6) {
+    console.log(`Passo ${passo}`)
+    passo ++
+}
+console.log("================")
+
+let corinthians = 1
+do {
+    console.log("Vai Corinthians!")
+    corinthians ++
+} while(corinthians  <= 10)
+console.log("----------------")
