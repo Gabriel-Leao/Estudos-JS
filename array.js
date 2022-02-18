8fb4f9b8c82212386a7e9f37edc189ffcc89264d
Gabriel-Leao <gabriellsilva1200@outlook.com.br>
2022-02-17 22:22:13 -0300

    Arrays

---
+++ array.js
@@ -0,0 +1,15 @@
+let num = [56,2,4,1,79,0]
+
+console.log("Meu vetor é " + num)
+// Coloca os itens na lista de modo decrescente
+console.log("Meu vetor organizado " + num.sort())
+
+for (let pos = 0; pos < num.length; pos++) {
+    console.log(`O item no índice ${pos} tem o valor de ${num[pos]}`)
+}
+
+let nomes = ["Gabriel", "Leão", "Sue", "Mayara", "Hugo", "Eliza"]
+
+for (let pos in nomes) {
+    console.log(`O nome na posição ${pos} é igual a ${nomes[pos]}`)
+}
