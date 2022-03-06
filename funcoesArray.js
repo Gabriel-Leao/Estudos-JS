99bc9222d2d5e07546c52f6a0d5f0aa362fa9a10
Gabriel-Leao <gabriellsilva1200@outlook.com.br>
2022-03-06 17:17:59 -0300

    funções de arrays

---
+++ funcoesArray.js
@@ -0,0 +1,44 @@
+const latinhas = ["Koka-kola", "Esprite"]
+
+latinhas.push("coleção-especial")
+
+// Filtar o array latinhas retornando os itens diferentes de "Esprite" e salva esse novo array dentro da const latinhasNaoEsprite
+const latinhasNaoEsprite = latinhas.filter(latinha => {
+    return latinha !== "Esprite"
+})
+
+// Ele procura no array e retorna o primeiro, e apenas o primeiro item que tiver "Koka-Kola" como valor. Quando ele achar o valor correspodente ele para de procurar 
+const latinhaKoka = latinhas.find(latinha => {
+    return latinha === "Koka-kola"
+})
+
+// Ele retorna um valor booleano, tem que passar o valor a ser encontrado, e tambem pode se passar de que posição ele começa a procurar
+const found = latinhas.includes("coleção-especial")
+
+// Retorna um novo array
+const novasLatinhas = [...latinhas, "Itubaína"]
+
+// Ele tira a primeira latinha e a última, e retorna os valores restantes em um novo array 
+const latinhasAtualizada = latinhas.slice(1, -1)
+
+// remove o ultimo item do array, modificando o array
+latinhas.pop()
+
+const fruits = ["🍎", "🍇"]
+
+// Transforma todos os itens dentro do array fruits em maçãs e gera um novo array
+const apple = fruits.map(fruta => {
+    return "🍎"
+})
+
+console.log(fruits)
+console.log(apple)
+
+// O reverse reverte a ordem dos itens no array. Ele altera o array original, então é preciso destruturar ele para que isso não aconteça
+const fruitsReverse = [...fruits].reverse()
+console.log(fruitsReverse)
+
+// O forEach() não retorna um novo array e nem altera o original, ele apenas faz algo para todos os itens dento do array. É um for para arrays
+fruits.forEach(fruit => {
+    console.log(fruit)
+})
