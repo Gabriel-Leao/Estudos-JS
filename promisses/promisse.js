dd9ce02cf4322baed17e4619d4c3b8e2a72be68a
Gabriel-Leao <gabriellsilva1200@outlook.com.br>
2022-03-29 19:18:33 -0300

    Promise

--- promisses/promisse.js
+++ promisses/promisse.js
@@ -1,5 +1,79 @@
+// Promises são como promessas da vida real, ela está esperando algum retorno
+// Para criar uma promessa intanciamos a classe promise.  Ela leva dois argumentos resole (solução) e reject (erro)
+const promessa = new Promise((resolve, reject) => {
+    const nome = "Gabriel"
+
+    if (nome === "Gabriel") {
+        resolve(`Usuário ${nome} encontrado`)
+    } else {
+        reject(`Usuário ${nome} não encontrado!`)
+    }
+})
+
+// para executar a promisse utilizamos o método then
+promessa.then((data) => {
+    console.log(data)
+})
+
+const promessa1 = new Promise((resolve, reject) => {
+    const nome = "Arnaldo"
+
+    if (nome === "Gabriel") {
+        resolve(`Usuário ${nome} encontrado`)
+    } else {
+        reject(`Usuário ${nome} não encontrado!`)
+    }
+})
+
+// Funciona como um try-catch
+promessa1.then((data) => {
+    console.log(data)
+}).catch((err) => {
+    console.log("Aconteceu um erro:", err)
+})
+
+// Resolver varias promessas utilizando o all, espera todas as promises estarem completas para retornar elas dentro de um array
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("P1 ok! Timeout")
+    }, 2000)
+})
+
+const p2 = new Promise((resolve, reject) => {
+    resolve("P2 ok!")
+})
+
+const p3 = new Promise((resolve, reject) => {
+   resolve("P3 ok!")
+})
+
+const resolveAll = Promise.all([p1, p2, p3]).then((data) => {
+    console.log(data)
+})
+
+console.log("Depois do all")
+
+// Resolver varias promessas utilizando o race, retorna a primeira promise concluida
+const p4 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("P4 ok! Timeout")
+    }, 2000)
+})
+
+const p5 = new Promise((resolve, reject) => {
+    resolve("P5 ok!")
+})
+
+const p6 = new Promise((resolve, reject) => {
+   resolve("P6 ok!")
+})
+
+const resolveRace = Promise.race([p4, p5, p6]).then((data) => {
+    console.log(data)
+})
+
 // Fetch request na API do Github
-// Fetch API
+// Fetch API retorna uma Promise
 const userName = "Gabriel-Leao"
 
 fetch(`https://api.github.com/users/${userName}`, {
@@ -8,12 +82,12 @@ fetch(`https://api.github.com/users/${userName}`, {
         accept: "application/vnd.github.v3+json"
     }
 })
+    // Para encadear mais processos utilizamos o método then
     .then((response) => {
         return response.json()
     })
     .then((data) => {
-        console.log(`Encontramos o usuario ${data.name}, ele tem ${data.followers} seguidores e mora em ${data.location}`)
-        console.log(data)
+        console.log(`Encontramos o usuario ${data.name}`)
         return data
     })
     .catch((err) => {
