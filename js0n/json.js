e7b4b46839d43253dfbf1321df099d18ad2a5269
Gabriel-Leao <gabriellsilva1200@outlook.com.br>
2022-03-30 14:34:24 -0300

    ajutes

--- js0n/json.js
+++ js0n/json.js
@@ -1,7 +1,7 @@
 const obj = {
     'nome': 'Gabriel',
     'idade': 18,
-    'esta__estudando': true,
+    'estudando': "Javascript",
     'hobies': ['ler', 'música', 'filmes', 'fazer amor'],
 }
 
@@ -10,5 +10,23 @@ console.log(typeof(obj))
 const json__obj = JSON.stringify(obj)
 console.log(json__obj)
 
+// Require não funciona no html
 let pessoas = require('./teste.json')
-console.log(pessoas)
+console.log(`O nome da pessoa é ${pessoas.nome} e ele tem ${pessoas.idade} anos`)
+
+let pessoa
+// fetch não funciona no node
+fetch("js0n/teste.json")
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        pessoa = data
+
+        function sortear(pessoa) {
+            let index = parseInt(Math.random() * pessoa.length)
+            console.log(`O usuario ${pessoa[index].nome} foi encontardo, ele tem ${pessoa[index].idade} anos.`)
+            return pessoa[index]
+        }
+        sortear(pessoa)
+    })
