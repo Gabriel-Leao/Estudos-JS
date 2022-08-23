9771e4ff8037091a485963676dbfb0d618248337
Gabriel-Leao <gabriel.leao2507@gmail.com>
2022-08-23 15:29:12 -0300

    Paíndromo

--- arnaldo/script.js
+++ arnaldo/script.js
@@ -1,17 +1,21 @@
-function inverte(word = "") {
+const inverte = (word = "") => {
   let invertido = ""
-  let tamanho = word.length
+  let tamanho = word.trim().length - 1
 
-  if (tamanho == 0) {
-    throw "Nenhuma palavra foi passada"
+  if (tamanho < 0) {
+    throw "Nenhuma palavra foi passada!"
+  } else if (tamanho == 0) {
+    throw "Por favor passe uma palavra ao invés de um caractere!"
   } else {
-    tamanho = tamanho - 1
     for (let i = tamanho; i >= 0; i--) {
       invertido += word[i]
     }
-  
-    return console.log(`A palavra ${word.toLocaleUpperCase().replace(/ /g, "")}, ao contrário é: ${invertido.toLocaleUpperCase().replace(/ /g, "")}.`)
   }
+  console.log(`A palavra "${word.replace(/ /g, "").toUpperCase()}" invertida é: "${invertido.replace(/ /g, "").toUpperCase()}".`)
+
+  if (word.replace(/ /g, "").toUpperCase() == invertido.replace(/ /g, "").toUpperCase()) {
+    return console.log("Essa palavra é um Palíndromo!")
+  } else return console.log("Essa palavra não é um Palíndromo!")
 }
 
-inverte("Gabriel")
+inverte("arara")
